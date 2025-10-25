f36c0298def55ab8b70297d7e6d79eb05b6905cb
lucasle-28 <lucasle5141@gmail.com>
2025-10-25 14:48:44 -0400

    File updated at 10/25/2025, 2:48:55 PM

--- 67-AddBinary/67-AddBinary.java
+++ 67-AddBinary/67-AddBinary.java
@@ -1,12 +1,13 @@
-// Last updated: 10/25/2025, 2:43:00 PM
+// Last updated: 10/25/2025, 2:48:55 PM
 class Solution {
-    public int reverseBits(int n) {
-        int res = 0;
-        for (int ii = 0; ii < 32; ii++) {
-            res <<= 1; //adds another space to occupy
-            res |= (n&1); //occupies that space with the last digit of n
-            n >>= 1; // shifts n to the right to clear the copied last digit
+    public int hammingWeight(int n) {
+        String strN = Integer.toBinaryString(n);
+        int count = 0;
+        for (int ii = 0; ii < strN.length();ii++) {
+            if (strN.charAt(ii) == '1') {
+                count++;
+            }
         }
-        return res;
+        return count;
     }
 }
