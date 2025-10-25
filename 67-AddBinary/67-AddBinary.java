f39c43dca33f32b3842c8b58530d18aae2c455a4
lucasle-28 <lucasle5141@gmail.com>
2025-10-25 14:42:49 -0400

    File updated at 10/25/2025, 2:43:00 PM

--- 67-AddBinary/67-AddBinary.java
+++ 67-AddBinary/67-AddBinary.java
@@ -1,32 +1,12 @@
-// Last updated: 10/25/2025, 2:37:47 PM
+// Last updated: 10/25/2025, 2:43:00 PM
 class Solution {
-    public String addBinary(String a, String b) {
-        StringBuilder res = new StringBuilder();
-        int ii = a.length() - 1;
-        int jj = b.length() - 1;
-        int carry = 0;
-        while (ii >= 0 || jj >= 0 || carry > 0) {
-            int intA;
-            int intB;
-            int sum = 0;
-            if (ii >= 0) {
-                intA = a.charAt(ii) - '0';
-            }
-            else {
-                intA = 0;
-            }
-            if (jj >= 0) {
-                intB = b.charAt(jj) - '0';
-            }
-            else {
-                intB = 0;
-            }
-            sum = intA + intB + carry;
-            res.append(sum%2);
-            carry = sum/2;
-            ii--;
-            jj--;
+    public int reverseBits(int n) {
+        int res = 0;
+        for (int ii = 0; ii < 32; ii++) {
+            res <<= 1; //adds another space to occupy
+            res |= (n&1); //occupies that space with the last digit of n
+            n >>= 1; // shifts n to the right to clear the copied last digit
         }
-        return res.reverse().toString();
+        return res;
     }
 }
